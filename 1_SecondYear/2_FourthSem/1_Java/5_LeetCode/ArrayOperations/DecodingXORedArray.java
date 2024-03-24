6c53dffc032c63ef0d2289decbd44890ee30e2f8
Vasudha <2023bit509@sggs.ac.in>
2024-03-24 10:03:07 +0530

    Java program to decode the encoded array using XOR operation

---
+++ 1_SecondYear/2_FourthSem/1_Java/5_LeetCode/ArrayOperations/DecodingXORedArray.java
@@ -0,0 +1,30 @@
+// 1720. Decode XORed Array
+
+class DecodingXORedArray {
+    public static void main(String[] args) {
+        DecodingXORedArray objArray = new DecodingXORedArray();
+        
+        int[] inputArray = {6,2,7,3};
+        int first = 4;
+        int[] outputArray = objArray.decode(inputArray, first);
+        for (int ele : outputArray) 
+            System.out.print(" " + ele);
+    }
+    
+    public int[] decode(int[] encoded, int first) {
+        int[] arr = new int[encoded.length+1];
+        arr[0] = first;
+        
+        for (int i = 0; i < encoded.length; i++) {
+            int j = 0;
+            while (true) {
+                if ((arr[i] ^ j) == encoded[i]) {
+                    arr[i+1] = j;
+                    break;
+                }
+                j++;
+            }
+        }
+        return arr;
+    }
+}
